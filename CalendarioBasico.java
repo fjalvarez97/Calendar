c65a7387161122d0ef9c7d8a8c437226f05cc407
Fran Alvarez <franjalvarez955@gmail.com>
2017-11-01 12:39:44 +0100

    He creado los campos dia mes año y he creado un constructor que les de valor 1

---
+++ CalendarioBasico.java
@@ -0,0 +1,25 @@
+/**
+ * This is a basic calendar.
+ * @Fran Álvarez DAM1
+ * @1/11/2017 
+ */
+public class CalendarioBasico
+{
+    // Day in the calendar
+    private int day;
+    // Month in the calendar
+    private int month;
+    // Year in the calendar
+    private int year;
+
+    /**
+     * Constructor for objects of class CalendarioBasico
+     */
+    public CalendarioBasico()
+    {
+        // Create an object of class CalendarioBasico
+        day = 01;
+        month = 01;
+        year = 01;
+    }
+}
